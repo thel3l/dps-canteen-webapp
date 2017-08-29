08fbc2a797cf432aa63b2b827c3aebe9bebc05b3
Infant Derrick Gnanasusairaj <infant.derrick.gnanasusairaj@gmail.com>
2017-08-29 23:31:18 +0530

    Integration of Firebase! v0.02a
    Finally! Integrated the firebase functionalities to the code and now the name, balance and the photo id completely depend on the firebase. Fixed some bugs on loading. Converted all the online links (except images) to offline links. Did more modifications on the Charset. However still getting error on Console: "[Deprecation] Synchronous XMLHttpRequest on the main thread is deprecated because of its detrimental effects to the end user's experience. For more help, check https://xhr.spec.whatwg.org/." Need to look more into this. However, this was a good update. P.S  - Cleaned asthetics of code, and deleted unused redunencies

--- firebaseInit.js
+++ firebaseInit.js
@@ -1,10 +1,10 @@
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyCdVFzknUKjtYtwvu2DWiuolhgcmG0_WbY",
-    authDomain: "test-a9f51.firebaseapp.com",
-    databaseURL: "https://test-a9f51.firebaseio.com",
-    projectId: "test-a9f51",
-    storageBucket: "",
-    messagingSenderId: "1042173971887"
+    apiKey: "AIzaSyCas9WSzPdCvatx7ODWMTquCCwiuZEj-UI",
+    authDomain: "dpsemca-1f00a.firebaseapp.com",
+    databaseURL: "https://dpsemca-1f00a.firebaseio.com",
+    projectId: "dpsemca-1f00a",
+    storageBucket: "dpsemca-1f00a.appspot.com",
+    messagingSenderId: "696186948502"
   };
   firebase.initializeApp(config);
