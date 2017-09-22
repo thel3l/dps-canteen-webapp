1c88a7efb8d4ee3cfc58a92d1b0dd36bcb32f557
Infant Derrick Gnanasusairaj <infant.derrick.gnanasusairaj@gmail.com>
2017-09-22 12:21:53 +0530

    Smooth Scroll
    Added the scrolling for the clock from the slideshow

--- js/script.js
+++ js/script.js
@@ -176,3 +176,9 @@ function transUpdate(){
     console.log(timestamp);
   });
 }
+$(".rchgnow").click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 1000, "easeOutCubic");
+    return false;
+});
