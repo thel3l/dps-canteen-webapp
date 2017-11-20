4efd0ce35d916a760d00ad2b1e4dade2b9e4fd94
Madrigal1 <30615210+madrigal1@users.noreply.github.com>
2017-11-20 12:20:14 +0530

    rupee symbol cross compatitbility

---
+++ dps-canteen-webapp-proto/main_script.js
@@ -0,0 +1,18 @@
+window.addEventListener("load",main);
+function main() {
+    //Death to frameworks !! 
+    var item=document.getElementsByClassName('items');
+    var screen = document.getElementById("tags");
+    var price=0;
+    var prices = [1,2,3,4,5,6,7,8,9,11,12,13,14,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42];
+
+    for(var i=0;i<item.length;i++) {
+          item[i].addEventListener("click",function() {
+               var e = parseInt(this.getAttribute('id'));
+               console.log(prices[e]); 
+                document.getElementById(this.getAttribute('id')).disabled =true;
+                price += prices[e];
+                  screen.innerHTML = price;
+         }); //end EventListener functionality
+    }
+}
