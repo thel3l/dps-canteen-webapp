94bec561c80aceeb72fd35e310ea2e9dee6803a0
Madrigal1 <kurupgokul11@gmail.com>
2017-11-23 16:05:25 +0530

    prices fixed

--- main_script.js
+++ main_script.js
@@ -59,7 +59,7 @@ function main() {
      source of the array could be in firebase ??
      prices.forEach(getFromFirebase)
     */ 
-    var prices = [1,2,3,4,5,6,7,8,9,11,12,13,14,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42];
+    var prices = [50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50];
 
     for(var i=0;i<item.length;i++) {
           item[i].addEventListener("click",function() {
