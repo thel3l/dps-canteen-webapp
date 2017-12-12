b492960c8da18911156d4b5e47ec0f1fa1a689dd
Gokul.J.Kurup <30615210+madrigal1@users.noreply.github.com>
2017-12-12 12:45:51 +0530

    cxvbc

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
- if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./sw.js')
-             .then(function() { console.log('Service Worker Registered'); });
-  }
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
