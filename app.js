56fbf5b468846dfe1b60b48a0d9c230f11798ffd
Gokul.J.Kurup <30615210+madrigal1@users.noreply.github.com>
2017-12-12 11:30:22 +0530

    eryh

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js', {
-    scope: '/'
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function() {
+    console.log("registered");
+  }).catch(function() {
+    console.log("failed");
   });
 }
