ed1e683d5839a5664f22356d905297e5ae54d887
Gokul.J.Kurup <30615210+madrigal1@users.noreply.github.com>
2017-12-30 15:51:19 +0530

    Sw active fetch setup

--- sw.js
+++ sw.js
@@ -19,3 +19,18 @@ self.addEventListener('install', function(event) {
       })
   );
 });
+
+//checks for user requests that are present in the cache
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
