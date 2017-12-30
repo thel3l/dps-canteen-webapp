a15e4caea15d73c34a810d4c76952e7a66c5cbf6
Gokul.J.Kurup <30615210+madrigal1@users.noreply.github.com>
2017-12-30 16:06:40 +0530

    Sw fetch h update

--- sw.js
+++ sw.js
@@ -21,16 +21,39 @@ self.addEventListener('install', function(event) {
 });
 
 //checks for user requests that are present in the cache
+//caches requests that the user does dynamically 
+//so no need to add all the routes above
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
-        // Cache hit - return response
         if (response) {
           return response;
         }
-        return fetch(event.request);
-      }
-    )
-  );
+
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
 });
