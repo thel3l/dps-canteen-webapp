2afe7a618878e00b40eec207ae8616f58478bd5d
Gokul.J.Kurup <30615210+madrigal1@users.noreply.github.com>
2017-12-31 14:51:44 +0530

    update on the https

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('./sw.js').then(function(registration) {
+    location.protocol === 'https:' && navigator.serviceWorker && navigator.serviceWorker.register('./sw.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
